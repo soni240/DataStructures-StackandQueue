4597189fc3f94b32ef1cc6fe95ba4297b18a07b4
soni240 <s.fegade21@gmail.com>
2022-06-29 22:02:38 +0530

    [Gunavanti]Add:Push Element In Stack

---
+++ StackandQueue/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackandQueue
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+}
+
